d970da1149050b92af476b61bdc1eace09fbfec2
prodgaraj@yandex.ru <prodgaraj@yandex.ru>
2021-10-14 00:59:39 +0300

    Added: Prescripteur class Patient class Fixed: Labels in Patient tab

---
+++ FuckYou/Patient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Patient
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public DateTime BirthDate { get; set; }
+
+    public Int64 NumeroSS { get; set; }
+
+    public string SSName { get; set; }
+
+    public string NAdh { get; set; }
+
+    public string Adress { get; set; }
+
+    public int TownIndex { get; set; }
+
+    public string Centre { get; set; }
+
+    public string TelephoneNumber { get; set; }
+
+    public string EMail { get; set; }
+
+    public bool Mutu { get; set; }
+
+    public string MutuNumber { get; set; }
+
+    public string Note { get; set; }
+
+    public Patient()
+    {
+
+    }
+}
---
+++ FuckYou/Prescripteur.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuckYou
+{
+    class Prescripteur
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Hospital { get; set; }
+
+        public string RPPS { get; set; }
+
+        public int Id { get; set; }
+
+        public int Finess { get; set; }
+
+        public string Spec { get; set; }
+
+
+
+        public Prescripteur()
+        {
+
+        }
+    }
+}
